c6bb36cb75e855cac10d240f7890186ee4ea1bec
Erik Svensson <eosvensson@gmail.com>
2019-02-11 08:16:36 +0100

    Decision form and changes to CaseOfficerService

---
+++ src/main/java/se/erik/lexicon/intra/entity/decision/DecisionForm.java
@@ -0,0 +1,73 @@
+package se.erik.lexicon.intra.entity.decision;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import se.erik.lexicon.intra.enums.DecisionType;
+
+public class DecisionForm {
+	
+	//Internal
+	
+	private String decisionId;
+	@NotBlank
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@PastOrPresent(message = "Beslutsdatum får inte vara i framtiden")
+	private LocalDate decisionDate;
+	@Positive(message = "Beslutslängden måste anges med en positive siffra")
+	private int durationInWeeks;
+	@NotNull(message = "Beslutstypen måste anges")
+	private DecisionType decisionType;
+	
+	//External	
+	private LocalDate endDate;
+	private int daysLeft;
+	
+	
+	public String getDecisionId() {
+		return decisionId;
+	}
+	public void setDecisionId(String decisionId) {
+		this.decisionId = decisionId;
+	}
+	public LocalDate getDecisionDate() {
+		return decisionDate;
+	}
+	public void setDecisionDate(LocalDate decisionDate) {
+		this.decisionDate = decisionDate;
+	}
+	public int getDurationInWeeks() {
+		return durationInWeeks;
+	}
+	public void setDurationInWeeks(int durationInWeeks) {
+		this.durationInWeeks = durationInWeeks;
+	}
+	public DecisionType getDecisionType() {
+		return decisionType;
+	}
+	public void setDecisionType(DecisionType decisionType) {
+		this.decisionType = decisionType;
+	}
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	public int getDaysLeft() {
+		return daysLeft;
+	}
+	public void setDaysLeft(int daysLeft) {
+		this.daysLeft = daysLeft;
+	}
+	
+	
+	
+
+}
--- src/main/java/se/erik/lexicon/intra/service/CaseOfficerService.java
+++ src/main/java/se/erik/lexicon/intra/service/CaseOfficerService.java
@@ -4,19 +4,31 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
+import org.springframework.scheduling.annotation.Async;
+
 import se.erik.lexicon.intra.entity.case_officer.CaseOfficer;
 import se.erik.lexicon.intra.entity.decision.Decision;
 
 public interface CaseOfficerService {
-	
+
 	CompletableFuture<Optional<CaseOfficer>> findByEmail(String email);
+
 	CompletableFuture<List<CaseOfficer>> findByCity(String city);
+
 	CompletableFuture<List<CaseOfficer>> findByPhoneNumber(String phoneNumber);
+
 	CompletableFuture<List<CaseOfficer>> findByFullNameContains(String name);
+
 	CompletableFuture<Optional<CaseOfficer>> findById(String id);
+
 	CompletableFuture<List<CaseOfficer>> findAll();
+
 	CompletableFuture<List<Decision>> fetchAllMadeDecisions(String officerId);
-	CaseOfficer save(CaseOfficer caseOfficer);
-	CaseOfficer update(CaseOfficer updated);
-	boolean delete(CaseOfficer caseOfficer);	
+
+	CaseOfficer save(CaseOfficer caseOfficer) throws IllegalArgumentException;
+
+	boolean delete(CaseOfficer caseOfficer) throws IllegalArgumentException;
+
+	CaseOfficer update(CaseOfficer updated) throws IllegalArgumentException;
+
 }
